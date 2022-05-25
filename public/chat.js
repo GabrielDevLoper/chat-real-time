32dd31fbad204c03338ffc2aa3eb3a25c8daaaf0
gabriebarreto <gabriel.devloper>
2022-05-25 11:22:47 -0300

    feat: implementando client basico para testar comunicação com o socket.

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,34 @@
 const socket = io();
+
+const urlSearch = new URLSearchParams(window.location.search);
+const username = urlSearch.get('username');
+const room = urlSearch.get('select_room');
+
+
+//emit => emitir alguma informação
+//on => escutando alguma informação
+socket.emit('select_room', {
+    username,
+    room
+});
+
+document.getElementById("message_input").addEventListener("keypress" , (event) => {
+    if(event.key === "Enter"){
+        const message = event.target.value;
+
+        const data = {
+            room,
+            username,
+            message
+        }
+
+        socket.emit("message", data);
+
+        event.target.value = "";
+    }
+});
+
+socket.on("message", response => {
+    console.log(response);
+});
+
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,58 @@
 import { io } from "./server";
 
+interface RoomUser {
+    socket_id: string;
+    username: string;
+    room: string;
+}
+
+interface Message {
+    username: string;
+    room: string;
+    message: string;
+    createdAt: Date;
+}
+
+const users: RoomUser[] = [];
+
+const messages: Message[] = [];
+
 io.on("connection", socket => {
-    console.log(socket.id);
+    
+    socket.on('select_room', response => {
+        const { room, username } = response;
+
+        //conectando em alguma sala
+        socket.join(room);
+
+        const userInRoom = users.find(user => user.username === username && user.room === room);
+
+        if(userInRoom){
+            userInRoom.socket_id = socket.id;
+        }else {
+            users.push({
+                room,
+                username,
+                socket_id: socket.id
+            });
+        }
+        
+        console.log(users);
+    });
+
+    socket.on("message", response => {
+        const { room, username, message: text } = response;
+
+        //Salvar as mensagens
+        const message: Message = {
+            room,
+            username,
+            message: text,
+            createdAt: new Date()
+        }
+
+        messages.push(message);
+        //Enviar para usuarios da sala
+        io.to(room).emit("message", message);
+    });
 });
