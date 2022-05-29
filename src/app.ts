af868286cdb10132c92eb711d94bcb0cbfc8d97a
gabriebarreto <gabriel.devloper>
2022-05-29 15:35:57 -0300

    feat: setando condição na porta

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 import { serverHttp } from "./server";
 import "./websocket";
 
-serverHttp.listen(3333, () => console.log("Servidor está rodando na porta 3333"));
+serverHttp.listen(process.env.PORT || 3333, () => console.log("Servidor está rodando na porta 3333"));
